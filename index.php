46a05458e218c118056fe52cfc49cd32f4d1241f
Frank Vinci <fv42wid@yahoo.com>
2014-07-07 00:05:11 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,138 @@
+<?php
+?>
+<!DOCTYPE html>
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>WP Theme 20140706</title>
+
+    <!-- Bootstrap -->
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="style.css" rel="stylesheet">
+
+</head>
+<body>
+<div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+ <div class="container">
+ 	<div class="row">
+ 		<div class="col-sm-3 nav-header-text">
+ 			<h2>Brand Info</h2>
+ 		</div>
+ 		
+ 		<div class="col-sm-9">
+ 			<div class="row">
+ 				<div class="col-sm-5 pull-right">
+ 					<p class="navbar-right phone-number-text">
+ 						<span class="glyphicon glyphicon-earphone"></span> (555)555-5555
+ 					</p>
+ 				</div>
+ 			</div>
+ 			<div class="row">
+ 				<nav>
+ 					<div class="collapse navbar-collapse pull-right" id="wp-navbar">
+						<ul class="nav navbar-nav">		
+		                <li><a href="#">HOME</a></li>		
+		                <li><a href="#">AGENCY</a></li>
+		                <li><a href="#">SERVICES</a></li>
+		                <li><a href="#">WORK</a></li>
+		                <li><a href="#">NEWSROOM</a></li>
+		                <li><a href="#">CONTACT</a></li>
+		                <li class="active"><a href="#">BLOG</a></li>
+		            	</ul>
+		        	</div>
+ 				</nav>
+ 			</div>
+ 		</div>
+ 	</div>
+ </div>
+</div>
+
+<div class="jumbotron text-center">
+	<p>- LET'S START PROMOTING -</p>
+	<h1>YOUR BUSINESS</h1>
+	<p>to improve customer leads into your website!</p>
+	<p><button type="button" class="btn btn-default">Let's talk now!</button></p>
+	
+	
+</div>
+
+<div class="row">
+	<!--  <div class="pull-left col-sm-1">
+		<img alt="blog image" class="img-responsive" src="http://placehold.it/125x100">
+	</div>-->
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-10">
+				<h3>BLOGS</h3>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="container">
+	<div class="row clearfix">
+		<div class="col-sm-12">
+			<div class="media">
+				<a class="pull-right" href="#">
+					<img alt="ad-image" src="http://placehold.it/230x150">
+				</a>
+				<div class="media-body">
+					<h4 class="media-heading">Blog Post 1</h4>
+					<p>
+					Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+					</p>
+					<button type="button" class="btn btn-default">READ MORE</button>
+					<small>posted by admin | 30th June, 2014 | (0) comments</small>
+				</div>
+			</div>
+			<div class="media">
+				<a class="pull-right" href="#">
+					<img alt="ad-image" src="http://placehold.it/230x150">
+				</a>
+				<div class="media-body">
+					<h4 class="media-heading">Blog Post 1</h4>
+					<p>
+					Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+					</p>
+					<button type="button" class="btn btn-default">READ MORE</button>
+					<small>posted by admin | 30th June, 2014 | (0) comments</small>
+				</div>
+			</div>
+			<div class="media">
+				<a class="pull-right" href="#">
+					<img alt="ad-image" src="http://placehold.it/230x150">
+				</a>
+				<div class="media-body">
+					<h4 class="media-heading">Blog Post 1</h4>
+					<p>
+					Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+					</p>
+					<button type="button" class="btn btn-default">READ MORE</button>
+					<small>posted by admin | 30th June, 2014 | (0) comments</small>
+				</div>
+			</div>
+		</div>
+		<!-- 
+		<div class="col-sm-3 ad-col">
+			<img alt="ad-image" src="http://placehold.it/230x150">
+			<img alt="ad-image" src="http://placehold.it/230x150">
+			<img alt="ad-image" src="http://placehold.it/230x150">
+			<img alt="ad-image" src="http://placehold.it/230x150">
+		
+		</div>
+		 -->
+	</div>
+	
+</div>
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="bootstrap/js/bootstrap.min.js"></script>
+
+</body>
+</html>
